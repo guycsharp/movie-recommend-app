cc8d68b25d04eca20ffb1f65e6b9a62e454d682c
sman40901 <sujanmanpr@gmail.com>
2025-01-17 01:23:14 -0800

    react is not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
+import ArticleTable from './ArticleTable';
 import './App.css'
 
 
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-
+      <ArticleTable/>
 
     </>
   )
---
+++ src/ArticleTable.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ArticleRow from "./ArticleRow";
+import articles from '../_data/your-articles.json'; // Update the path to your JSON file
+
+function ArticleTable() {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Image</th>
+            <th>Link</th>
+            <th>Author Name</th>
+            <th>Author Image</th>
+            <th>Posted Date</th>
+            <th>Minutes to Read</th>
+            <th>Has Audio Available</th>
+            <th>Member Preview</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles.map((article, index) => (
+            <ArticleRow key={index} {...article} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default ArticleTable;
