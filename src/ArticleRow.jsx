0fd91731c677525e036f76d45a0055dfcaea86ef
sman40901 <sujanmanpr@gmail.com>
2025-01-17 01:09:43 -0800

    syntax corrected

---
+++ src/ArticleRow.jsx
@@ -0,0 +1,33 @@
+function ArticleRow(props) {
+  const {
+    title,
+    description,
+    image,
+    link,
+    authorName,
+    authorImage,
+    postedDate,
+    minutesToRead,
+    hasAudioAvailable,
+    memberPreview,
+    altImage,
+    altAuthorImage
+  } = props;
+
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{description}</td>
+      <td><img src={image} alt={altImage} /></td>
+      <td><a href={link}>Read Article</a></td>
+      <td>{authorName}</td>
+      <td><img src={authorImage} alt={altAuthorImage} /></td>
+      <td>{postedDate}</td>
+      <td>{minutesToRead}</td>
+      <td>{hasAudioAvailable}</td>
+      <td>{memberPreview}</td>
+    </tr>
+  );
+}
+
+export default ArticleRow;
